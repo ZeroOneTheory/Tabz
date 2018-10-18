b301001f4672d84664b0c901fdec84d8f774ad9b
ZeroOneTheory <36996656+ZeroOneTheory@users.noreply.github.com>
2018-10-18 17:54:40 -0400

    Animating

---
+++ New Unity Project/Assets/Scripts/GuestAnimator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuestAnimator : MonoBehaviour {
+
+    Animator anim;
+    [Range(0,2)]
+    [SerializeField] int animNum;
+
+    private void Awake() {
+        anim = GetComponent<Animator>();
+    }
+    private void Update() {
+        if (GameManager.Instance.InputController.tempKey) {
+            if (animNum == int.MaxValue) {
+                animNum = 0;
+            } else { animNum += 1; }
+            
+        }
+    }
+}
--- New Unity Project/Assets/Scripts/InputController.cs
+++ New Unity Project/Assets/Scripts/InputController.cs
@@ -10,11 +10,13 @@ public class InputController : MonoBehaviour {
     public bool leftMouse;
     public Vector2 mouseInput;
     public Vector2 inputDir;
+    public bool tempKey;
 
     private void Update() {
         vertical = Input.GetAxisRaw("Vertical");
         horizontal = Input.GetAxisRaw("Horizontal");
         mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
         leftMouse = Input.GetMouseButtonDown(0);
+        tempKey = Input.GetKeyDown(KeyCode.Z);
     }
 }
