a5e7da95296aaf310e9df8e879fc3740f83824b8
ZeroOneTheory <36996656+ZeroOneTheory@users.noreply.github.com>
2018-10-18 13:30:36 -0400

    Setting up Camera

---
+++ New Unity Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    InputController inputControl;
+
+    [SerializeField] float pitch;
+    [SerializeField] float yaw;
+
+    private void Awake() {
+        inputControl = GameManager.Instance.InputController;
+    }
+
+    private void Update() {
+        yaw = inputControl.mouseInput.y;
+    }
+}
