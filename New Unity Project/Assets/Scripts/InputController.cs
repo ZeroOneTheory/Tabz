bbb91029c2bcb6aa228bacb6019fb44f576a59df
ZeroOneTheory <36996656+ZeroOneTheory@users.noreply.github.com>
2018-10-18 12:58:48 -0400

    New Project

---
+++ New Unity Project/Assets/Scripts/InputController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour {
+
+    public float vertical;
+    public float horizontal;
+    public float scrollZoom;
+    public Vector2 mouseInput;
+    public Vector2 inputDir;
+
+    private void Update() {
+        vertical = Input.GetAxisRaw("Vertical");
+        horizontal = Input.GetAxisRaw("Horizontal");
+        mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        Debug.Log("Mouse Axis" + mouseInput);
+    }
+}
